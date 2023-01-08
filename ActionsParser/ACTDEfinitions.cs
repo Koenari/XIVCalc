b3b4f10d4a714c0651e11237847dbce096393dc7
Koenari <koenari@koenari.de>
2023-01-08 14:17:23 +0100

    First proof of concept

---
+++ ActionsParser/ACTDEfinitions.cs
@@ -0,0 +1,42 @@
+﻿namespace ActionsParser
+{
+    internal class ACTDefinitions
+    {
+        public ACTDefinitions()
+        {
+
+        }
+    }
+    public class DamageItem
+    {
+        public int potency { get; set; }
+    }
+
+    public class ActionsItem
+    {
+        public KeyValuePair<string, string> info { get; set; }
+        public List<DamageItem> damage { get; set; }
+        public List<DamageItem> heal { get; set; }
+    }
+
+    public class Timeproc
+    {
+        public string type { get; set; }
+        public string potency { get; set; }
+        public string maxticks { get; set; }
+    }
+
+    public class StatuseffectsItem
+    {
+        public KeyValuePair<string, string> action { get; set; }
+        public Timeproc timeproc { get; set; }
+    }
+
+    public class ActJobDefnition
+    {
+        public string job { get; set; }
+        public List<ActionsItem> actions { get; set; }
+        public List<StatuseffectsItem> statuseffects { get; set; }
+    }
+
+}
