4403593df7dfc42bf3e5541a4ce90cf64654c988
Koenari <koenari@koenari.de>
2023-03-05 10:22:15 +0100

    finish ACT json definitions

--- ActionsParser/ACTDEfinitions.cs
+++ ActionsParser/ACTDEfinitions.cs
@@ -1,42 +1,120 @@
-﻿namespace ActionsParser
+﻿using System.Globalization;
+
+namespace XivCalc.ActionsParser.ActDefinitions;
+
+
+#region RawDefinitions
+#pragma warning disable IDE1006 // Naming Styles
+public enum DamageType
 {
-    internal class ACTDefinitions
-    {
-        public ACTDefinitions()
-        {
-
-        }
-    }
-    public class DamageItem
-    {
-        public int potency { get; set; }
-    }
-
-    public class ActionsItem
-    {
-        public KeyValuePair<string, string> info { get; set; }
-        public List<DamageItem> damage { get; set; }
-        public List<DamageItem> heal { get; set; }
-    }
-
-    public class Timeproc
-    {
-        public string type { get; set; }
-        public string potency { get; set; }
-        public string maxticks { get; set; }
-    }
-
-    public class StatuseffectsItem
-    {
-        public KeyValuePair<string, string> action { get; set; }
-        public Timeproc timeproc { get; set; }
-    }
-
-    public class ActJobDefnition
-    {
-        public string job { get; set; }
-        public List<ActionsItem> actions { get; set; }
-        public List<StatuseffectsItem> statuseffects { get; set; }
-    }
-
+    Unknown,
+    Magic,
+    Physical
 }
+public class RawDamageItem
+{
+    public int potency { get; set; }
+    public int combo { get; set; }
+    public int targetindex { get; set; }
+}
+
+public class ActionsItem
+{
+    public int ID => int.Parse($"0x{info.Key}", NumberStyles.HexNumber);
+    public string Name => info.Value;
+    public KeyValuePair<string, string> info { get; set; }
+    public List<RawDamageItem> damage { get; set; }
+    public List<RawDamageItem> heal { get; set; }
+}
+
+public abstract class StatusEffectBase<T> where T : struct
+{
+    public T ParsedType => Enum.TryParse(type, true, out T parsed) ? parsed : default;
+    public string? type { get; set; }
+}
+
+public class TimeprocEffect : StatusEffectBase<TimeprocEffect.Type>
+{
+    public int potency { get; set; }
+    public string? damagetype { get; set; }
+    public DamageType ParsedDmgType => Enum.TryParse(damagetype, true, out DamageType parsed) ? parsed : DamageType.Unknown;
+    public int maxticks { get; set; }
+    public enum Type
+    {
+        Unknown,
+        DoT,
+        HoT,
+    }
+}
+public class PotencyEffect : StatusEffectBase<PotencyEffect.Type>
+{
+    public int potency { get; set; }
+    public int amount { get; set; }
+    public string? limittodamagetype { get; set; }
+    public bool? isstacked { get; set; }
+    public DamageType limitedTo => Enum.TryParse(limittodamagetype, true, out DamageType limited) ? limited : DamageType.Unknown;
+
+    public enum Type
+    {
+        Unknown,
+        GroundHeal,
+        GroundDamage,
+        HealReceiveMultiplier,
+        DamageReceivedMultiplier,
+        HealDoneMultiplier,
+        DamageDoneMultiplier
+    }
+}
+public class DamageShieldEffect : StatusEffectBase<DamageShieldEffect.Type>
+{
+    public int amount { get; set; }
+    public enum Type
+    {
+        Unknown,
+        Potency,
+        TargetHpPercent,
+        HealPercent
+    }
+}
+public class MultiplierEffect : StatusEffectBase<MultiplierEffect.Type>
+{
+    public int amount { get; set; }
+    public string? limitto { get; set; }
+
+    public enum Type
+    {
+        Unknown,
+        CriticalHit,
+        CriticalReceived,
+        DirectHit,
+    }
+}
+public class ReactiveProcEffect : StatusEffectBase<ReactiveProcEffect.Type>
+{
+    public enum Type
+    {
+        Unknown,
+        HealOnHealCast,
+        HealOnDamageDealt,
+    }
+}
+public class StatuseffectsItem
+{
+    public int ID => int.TryParse($"0x{info.Key}", NumberStyles.HexNumber, null, out int parsed) ? parsed : -1;
+    public string Name => info.Value;
+    public KeyValuePair<string, string> info { get; set; }
+    public TimeprocEffect? timeproc { get; set; }
+    public List<PotencyEffect>? potency { get; set; }
+    public DamageShieldEffect? damageshield { get; set; }
+    public List<MultiplierEffect>? multiplier { get; set; }
+    public ReactiveProcEffect? reactiveproc { get; set; }
+}
+
+public class ActJobDefnition
+{
+    public string job { get; set; }
+    public List<ActionsItem> actions { get; set; }
+    public List<StatuseffectsItem> statuseffects { get; set; }
+}
+#pragma warning restore IDE1006 // Naming Styles
+#endregion
--- ActionsParser/Program.cs
+++ ActionsParser/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Lumina;
 using Lumina.Excel;
 using Lumina.Excel.GeneratedSheets;
 using System.Collections.Concurrent;
@@ -7,111 +8,121 @@ using System.Collections.Immutable;
 using System.Globalization;
 using Action = Lumina.Excel.GeneratedSheets.Action;
 
-if (args.Length != 1)
-{
-    PrintUsage();
-    return 1;
-}
-ConcurrentDictionary<uint, (string name, byte cat, float recast, string potency)> output = new();
-int errorcount = 0;
-int emptyCount = 0;
-int correctCount = 0;
-try
-{
-    ExcelModule Exclemodule = new(new(args[0] + "/game/sqpack"));
-    ExcelSheet<Action>? ActionSheet = Exclemodule.GetSheet<Action>();
-    ExcelSheet<ActionTransient>? actionTransients = Exclemodule.GetSheet<ActionTransient>();
-    if (actionTransients is null || ActionSheet is null)
-        throw new NullReferenceException();
-    Console.WriteLine("Sheets loaded");
+namespace XivCalc.ActionsParser;
 
-    Parallel.ForEach<Action>(ActionSheet, (Action a) =>
+public class Program
+{
+
+    public static int Main(string[] args)
     {
-        if (a.ActionCategory.Row < 2 || a.ActionCategory.Row > 4)
-            return;
-        if (!a.IsPlayerAction)
-            return;
-        string pot = ParseDescription(actionTransients.GetRow(a.RowId)?.Description.RawString ?? "");
-        output[a.RowId] = (a.Name, (byte)a.ActionCategory.Row, a.Recast100ms / 10f, pot);
-    });
-    using (StreamWriter writer = new StreamWriter("output.cs"))
-    {
-        writer.WriteLine("Dictionary<uint, (byte Category, float Reccast, int Potency)> ActionDB = new();");
-        writer.WriteLine("ActionDB = new()");
-        writer.WriteLine("{");
-        foreach (var item in output.ToImmutableSortedDictionary())
+        if (args.Length != 1)
         {
-            var (name, cat, recast, potency) = item.Value;
-            writer.WriteLine("    " + formatOuputLine(item.Key, name, cat, recast, potency));
+            PrintUsage();
+            return 1;
         }
-        writer.WriteLine("}");
-    }
-    Console.WriteLine($"Complex Potencies:{errorcount}");
-    Console.WriteLine($"Empty Potencies:{emptyCount}");
-    Console.WriteLine($"Correct Potencies:{correctCount}");
-
-    return 0;
-}
-catch (IOException)
-{
-    Console.WriteLine("Wrong Game Path");
-    return 1;
-}
-catch (NullReferenceException)
-{
-    Console.WriteLine("Lumina error");
-    return 1;
-}
-
-string ParseDescription(string description)
-{
-    string result = "";
-    int idx = description.IndexOf("potency of");
-    //No potency
-    if (idx == -1)
-    {
-        Interlocked.Increment(ref emptyCount);
-        return "0";//$"0/*{description}*/";
-    }
-
-    string pot = description.Substring(idx + 11);
-    //Simple number
-    if (pot[0] >= '0' && pot[0] <= '9')
-    {
+        ConcurrentDictionary<uint, (string name, byte cat, float recast, string potency)> output = new();
+        int errorcount = 0;
+        int emptyCount = 0;
+        int correctCount = 0;
         try
         {
-            char[] ends = new char[] { '.', ' ' };
-            int idx2 = pot.IndexOfAny(ends);
-            if (idx2 == -1)
-                throw new FormatException();
-            pot = pot.Remove(idx2);
-            IFormatProvider provider = CultureInfo.CreateSpecificCulture("en-US");
-            result = $"{int.Parse(pot, NumberStyles.AllowThousands, provider)}";
+            GameData gdata = new(args[0] + "/game/sqpack");
+            gdata.Options.PanicOnSheetChecksumMismatch = false;
+            ExcelModule Exclemodule = new(gdata);
+            ExcelSheet<Action>? ActionSheet = Exclemodule.GetSheet<Action>();
+            ExcelSheet<ActionTransient>? actionTransients = Exclemodule.GetSheet<ActionTransient>();
+            if (actionTransients is null || ActionSheet is null)
+                throw new NullReferenceException();
+            Console.WriteLine("Sheets loaded");
+
+            Parallel.ForEach<Action>(ActionSheet, (Action a) =>
+            {
+                if (a.ActionCategory.Row < 2 || a.ActionCategory.Row > 4)
+                    return;
+                if (!a.IsPlayerAction)
+                    return;
+                string pot = ParseDescription(actionTransients.GetRow(a.RowId)?.Description.RawString ?? "");
+                output[a.RowId] = (a.Name, (byte)a.ActionCategory.Row, a.Recast100ms / 10f, pot);
+            });
+            using (StreamWriter writer = new StreamWriter("output.cs"))
+            {
+                writer.WriteLine("Dictionary<uint, (byte Category, float Reccast, int Potency)> ActionDB = new();");
+                writer.WriteLine("ActionDB = new()");
+                writer.WriteLine("{");
+                foreach (var item in output.ToImmutableSortedDictionary())
+                {
+                    var (name, cat, recast, potency) = item.Value;
+                    writer.WriteLine("    " + formatOuputLine(item.Key, name, cat, recast, potency));
+                }
+                writer.WriteLine("}");
+            }
+            Console.WriteLine($"Complex Potencies:{errorcount}");
+            Console.WriteLine($"Empty Potencies:{emptyCount}");
+            Console.WriteLine($"Correct Potencies:{correctCount}");
+
+            return 0;
         }
-        catch (FormatException)
+        catch (IOException)
         {
-            result = $"0/*{pot}*/";
+            Console.WriteLine("Wrong Game Path");
+            return 1;
         }
-        Interlocked.Increment(ref correctCount);
-        return result;
+        catch (NullReferenceException)
+        {
+            Console.WriteLine("Lumina error");
+            return 1;
+        }
+        string ParseDescription(string description)
+        {
+            string result = "";
+            int idx = description.IndexOf("potency of");
+            //No potency
+            if (idx == -1)
+            {
+                Interlocked.Increment(ref emptyCount);
+                return "0";//$"0/*{description}*/";
+            }
+
+            string pot = description.Substring(idx + 11);
+            //Simple number
+            if (pot[0] >= '0' && pot[0] <= '9')
+            {
+                try
+                {
+                    char[] ends = new char[] { '.', ' ' };
+                    int idx2 = pot.IndexOfAny(ends);
+                    if (idx2 == -1)
+                        throw new FormatException();
+                    pot = pot.Remove(idx2);
+                    IFormatProvider provider = CultureInfo.CreateSpecificCulture("en-US");
+                    result = $"{int.Parse(pot, NumberStyles.AllowThousands, provider)}";
+                }
+                catch (FormatException)
+                {
+                    result = $"0/*{pot}*/";
+                }
+                Interlocked.Increment(ref correctCount);
+                return result;
+            }
+            //Complex Expression
+            //PlayerParameter(68) = ClassJob
+            //PlayerParameter(72) = Level
+
+            result = $"\"{pot}\"";
+            Interlocked.Increment(ref errorcount);
+
+
+
+            return result;
+        }
+
+        void PrintUsage()
+        {
+
+        }
+        string formatOuputLine(uint id, string name, byte category, float recast, string potency)
+        {
+            return $"[{id}] = (\"{name}\", {category}, {recast}f, {potency})";
+        }
     }
-    //Complex Expression
-    //PlayerParameter(68) = ClassJob
-    //PlayerParameter(72) = Level
-
-    result = $"\"{pot}\"";
-    Interlocked.Increment(ref errorcount);
-
-
-
-    return result;
-}
-
-void PrintUsage()
-{
-
-}
-string formatOuputLine(uint id, string name, byte category, float recast, string potency)
-{
-    return $"[{id}] = (\"{name}\", {category}, {recast}f, {potency})";
 }
