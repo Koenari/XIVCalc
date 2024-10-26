52ca34b2175d47646b80915a369a3a7b0049a4ac
Mira <koenari@koenari.de>
2024-10-26 21:35:55 +0200

    Add Lumina Compatibility project

---
+++ XIVCalc.Lumina/LuminaJobModifiers.cs
@@ -0,0 +1,22 @@
+﻿using Lumina.Excel.GeneratedSheets;
+using XIVCalc.Interfaces;
+
+namespace XIVCalc.Lumina;
+
+public class LuminaJobModifiers(ClassJob source) : IJobModifiers
+{
+        public Job Job => (Job)source.RowId;
+        public int ModifierHitPoints => source.ModifierHitPoints;
+        public int ModifierManaPoints => source.ModifierManaPoints;
+        public int ModifierStrength => source.ModifierStrength;
+        public int ModifierVitality => source.ModifierVitality;
+        public int ModifierDexterity => source.ModifierDexterity;
+        public int ModifierIntelligence => source.ModifierIntelligence;
+        public int ModifierMind => source.ModifierMind;
+        public int ModifierPiety => source.ModifierMind;
+        public bool IsTank => Job.IsTank();
+        public StatType PrimaryStat => (StatType)source.PrimaryStat;
+        public bool IsCaster => Job.IsCaster();
+
+        public static implicit operator LuminaJobModifiers(ClassJob job) => new(job);
+}
