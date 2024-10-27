4f5fe98e648ae5fa8683c543572982a06b069682
Mira <koenari@koenari.de>
2024-10-27 19:07:39 +0100

    Add missing jobs

--- XIVCalc/Enums.cs
+++ XIVCalc/Enums.cs
@@ -13,89 +13,13 @@ public enum Job : byte
     /// </summary>
     ADV = 0,
     /// <summary>
-    /// Astrologian
-    /// </summary>
-    AST = 33,
-    /// <summary>
-    /// Black MAge
-    /// </summary>
-    BLM = 25,
-    /// <summary>
-    /// Blue Mage
-    /// </summary>
-    BLU = 36,
-    /// <summary>
-    /// Bard
-    /// </summary>
-    BRD = 23,
-    /// <summary>
-    /// Dancer
-    /// </summary>
-    DNC = 38,
-    /// <summary>
-    /// Dragoon
-    /// </summary>
-    DRG = 22,
-    /// <summary>
-    /// Dark Knight
-    /// </summary>
-    DRK = 32,
-    /// <summary>
-    /// Gunbreaker
-    /// </summary>
-    GNB = 37,
-    /// <summary>
-    /// Machinist
-    /// </summary>
-    MCH = 31,
-    /// <summary>
-    /// Monk
-    /// </summary>
-    MNK = 20,
-    /// <summary>
-    /// Ninja
-    /// </summary>
-    NIN = 30,
-    /// <summary>
-    /// Paladin
-    /// </summary>
-    PLD = 19,
-    /// <summary>
-    /// Redmage
-    /// </summary>
-    RDM = 35,
-    /// <summary>
-    /// Reaper
-    /// </summary>
-    RPR = 39,
-    /// <summary>
-    /// Samurai
-    /// </summary>
-    SAM = 34,
-    /// <summary>
-    /// Scholar
-    /// </summary>
-    SCH = 28,
-    /// <summary>
-    /// Sage
-    /// </summary>
-    SGE = 40,
-    /// <summary>
-    /// Summoner
-    /// </summary>
-    SMN = 27,
-    /// <summary>
-    /// Warrior
-    /// </summary>
-    WAR = 21,
-    /// <summary>
-    /// White Mage
-    /// </summary>
-    WHM = 24,
-    /// <summary>
     /// Gladiator
     /// </summary>
     GLA = 1,
+    /// <summary>
+    /// Pugilist
+    /// </summary>
+    PGL = 2,
     /// <summary>
     /// Marauder
     /// </summary>
@@ -105,29 +29,149 @@ public enum Job : byte
     /// </summary>
     LNC = 4,
     /// <summary>
-    /// Pugilist
-    /// </summary>
-    PGL = 2,
-    /// <summary>
     /// Archer
     /// </summary>
     ARC = 5,
-    /// <summary>
-    /// Thaumaturge
-    /// </summary>
-    THM = 7,
-    /// <summary>
-    /// Arcanist
-    /// </summary>
-    ACN = 26,
     /// <summary>
     /// Conjurer
     /// </summary>
     CNJ = 6,
     /// <summary>
+    /// Thaumaturge
+    /// </summary>
+    THM = 7,
+    /// <summary>
+    /// Carpenter
+    /// </summary>
+    CRP = 8,
+    /// <summary>
+    /// Blacksmith
+    /// </summary>
+    BSM = 9,
+    /// <summary>
+    /// Armorer
+    /// </summary>
+    ARM = 10,
+    /// <summary>
+    /// Goldsmith
+    /// </summary>
+    GSM = 11,
+    /// <summary>
+    /// Leatherworker
+    /// </summary>
+    LTW = 12,
+    /// <summary>
+    /// Weaver
+    /// </summary>
+    WVR = 13,
+    /// <summary>
+    /// Alchemist
+    /// </summary>
+    ALC = 14,
+    /// <summary>
+    /// Culinarian
+    /// </summary>
+    CUL = 15,
+    /// <summary>
+    /// Miner
+    /// </summary>
+    MIN = 16,
+    /// <summary>
+    /// Botanist
+    /// </summary>
+    BTN = 17,
+    /// <summary>
+    /// Fisher
+    /// </summary>
+    FSH = 18,
+    /// <summary>
+    /// Paladin
+    /// </summary>
+    PLD = 19,
+    /// <summary>
+    /// Warrior
+    /// </summary>
+    WAR = 21,
+    /// <summary>
+    /// Monk
+    /// </summary>
+    MNK = 20,
+    /// <summary>
+    /// Dragoon
+    /// </summary>
+    DRG = 22,
+    /// <summary>
+    /// Bard
+    /// </summary>
+    BRD = 23,
+    /// <summary>
+    /// White Mage
+    /// </summary>
+    WHM = 24,
+    /// <summary>
+    /// Black MAge
+    /// </summary>
+    BLM = 25,
+    /// <summary>
+    /// Arcanist
+    /// </summary>
+    ACN = 26,
+    /// <summary>
+    /// Summoner
+    /// </summary>
+    SMN = 27,
+    /// <summary>
+    /// Scholar
+    /// </summary>
+    SCH = 28,
+    /// <summary>
     /// Rogue
     /// </summary>
     ROG = 29,
+    /// <summary>
+    /// Ninja
+    /// </summary>
+    NIN = 30,
+    /// <summary>
+    /// Machinist
+    /// </summary>
+    MCH = 31,
+    /// <summary>
+    /// Dark Knight
+    /// </summary>
+    DRK = 32,
+    /// <summary>
+    /// Astrologian
+    /// </summary>
+    AST = 33,
+    /// <summary>
+    /// Samurai
+    /// </summary>
+    SAM = 34,
+    /// <summary>
+    /// Redmage
+    /// </summary>
+    RDM = 35,
+    /// <summary>
+    /// Blue Mage
+    /// </summary>
+    BLU = 36,
+    /// <summary>
+    /// Gunbreaker
+    /// </summary>
+    GNB = 37,
+    /// <summary>
+    /// Dancer
+    /// </summary>
+    DNC = 38,
+    /// <summary>
+    /// Reaper
+    /// </summary>
+    RPR = 39,
+    /// <summary>
+    /// Sage
+    /// </summary>
+    SGE = 40,
     /// <summary>
     /// Viper
     /// </summary>
